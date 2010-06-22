067594b6bebac2d71a734ee5d34af2d3975c0ab7
jbruce <jbruce@medley.(none)>
2010-06-22 13:50:29 -0400

    - made stopwords unique

--- taggit/contrib/stopwords/models.py
+++ taggit/contrib/stopwords/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class StopWord(models.Model):
     """Model to store stop-words 
        which can never be used as a tag/topic"""
-    stopword = models.CharField(max_length=200)
+    stopword = models.CharField(max_length=200, unique=True)
 
     def __unicode__(self):
         return self.stopword
